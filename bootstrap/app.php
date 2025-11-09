b54585e166d2446730ece924fc94c430a23f980e
karshfish <87549809+karshfish@users.noreply.github.com>
2025-11-09 19:24:09 +0200

    this the app fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'role' => RoleMiddleware::class
-
-
+            'role' => RoleMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
